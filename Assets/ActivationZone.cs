b8a65e4937eb96d127150f0ba55bab947386a179
jfcwts <lawrencke@gmail.com>
2026-02-01 12:58:17 +0100

    spot light added to activation zone

--- Assets/ActivationZone.cs
+++ Assets/ActivationZone.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class ActivationZone : MonoBehaviour
 {
     [SerializeField] private float worldRadius;
+    private Light2D light2D;
 
     void Awake()
     {
@@ -12,6 +14,14 @@ public class ActivationZone : MonoBehaviour
         float localSpriteRadius = spriteRenderer.sprite.bounds.extents.x;
         float scaleFactor = worldRadius / localSpriteRadius;
         transform.localScale = new Vector3(scaleFactor, scaleFactor, 1f);
+        light2D = GetComponent<Light2D>();
+    }
+
+    void Start()
+    {
+        light2D.pointLightInnerRadius = Mathf.Max(0,worldRadius - 1);
+        light2D.pointLightOuterRadius = worldRadius;
+
     }
 
     public float GetWorldRadius()
